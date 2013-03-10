223104a323c1dbfeb2a5f7bb7e88e53908b2024f
Guido Steinacker <guido.steinacker@gmail.com>
2013-03-10 20:55:08 +0100

    Updated copyright notice.

--- jsonhome-generator/src/main/java/de/otto/jsonhome/generator/UriTemplateHelper.java
+++ jsonhome-generator/src/main/java/de/otto/jsonhome/generator/UriTemplateHelper.java
@@ -1,11 +1,11 @@
 /*
- * Copyright 2002-2011 the original author or authors.
+ * Copyright 2012 Guido Steinacker
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *      http://www.apache.org/licenses/LICENSE-2.0
+ * http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -17,12 +17,9 @@ package de.otto.jsonhome.generator;
 
 import java.io.Serializable;
 import java.util.ArrayList;
+import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
-
-import java.util.Collections;
-import java.util.LinkedList;
-import java.util.List;
 
 /**
  * Represents a URI template. A URI template is a URI-like String that contains variables enclosed
--- jsonhome-registry/src/main/java/de/otto/jsonhome/registry/controller/RegistriesController.java
+++ jsonhome-registry/src/main/java/de/otto/jsonhome/registry/controller/RegistriesController.java
@@ -1,18 +1,19 @@
-/**
- Copyright 2012 Guido Steinacker
-
- Licensed under the Apache License, Version 2.0 (the "License");
- you may not use this file except in compliance with the License.
- You may obtain a copy of the License at
-
- http://www.apache.org/licenses/LICENSE-2.0
-
- Unless required by applicable law or agreed to in writing, software
- distributed under the License is distributed on an "AS IS" BASIS,
- WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- See the License for the specific language governing permissions and
- limitations under the License.
+/*
+ * Copyright 2012 Guido Steinacker
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
+
 package de.otto.jsonhome.registry.controller;
 
 import de.otto.jsonhome.annotation.Doc;
@@ -28,7 +29,6 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
 import java.net.URI;
 import java.util.Map;
 
@@ -38,7 +38,6 @@ import static de.otto.jsonhome.registry.controller.RegistryConverter.registryToJ
 import static java.net.URI.create;
 import static javax.servlet.http.HttpServletResponse.*;
 import static org.springframework.http.HttpStatus.BAD_REQUEST;
-import static org.springframework.http.HttpStatus.NOT_FOUND;
 
 /**
  * Controller responsible for requests to the <code>/registry</code> resource.
